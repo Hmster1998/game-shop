4bf891dc48a8dc8c842330a579a558f577ca3ddf
Hmster1998 <72091670+Hmster1998@users.noreply.github.com>
2025-02-19 00:04:15 +0700

    Update server.php
    ลบSpace

--- server.php
+++ server.php
@@ -1,6 +1,6 @@
 <?php
 $url = parse_url(getenv("mysql://c21m3bihcvb3u8ib:lj70kn5jrdaxl91v@ijj1btjwrd3b7932.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/d99c5oiieri85ca1"));
-$host = $url["ijj1btjwrd3b7932.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	"];
+$host = $url["ijj1btjwrd3b7932.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"];
 $user = $url["c21m3bihcvb3u8ib"];
 $pass = $url["lj70kn5jrdaxl91v"];
 $dbname = substr($url["d99c5oiieri85ca1"], 1);
