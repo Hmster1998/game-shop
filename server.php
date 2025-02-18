1c12853dc905f6d7470dcc77fc1a854dc394ab43
Hmster1998 <72091670+Hmster1998@users.noreply.github.com>
2025-02-19 00:18:45 +0700

    Update server.php

--- server.php
+++ server.php
@@ -1,9 +1,11 @@
 <?php
-$url = parse_url(getenv(" mysql://vuhsxgawmm76q2m0:cpe4lruxq8gtnexs@ijj1btjwrd3b7932.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/h335cz5ylaqekthf"));
+// ใช้ getenv() เพื่อดึงค่า JAWSDB_URL ที่ Heroku ตั้งไว้
+$url = parse_url(getenv("mysql://vuhsxgawmm76q2m0:cpe4lruxq8gtnexs@ijj1btjwrd3b7932.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/h335cz5ylaqekthf"));
 $host = $url["ijj1btjwrd3b7932.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"];
-$user = $url["c21m3bihcvb3u8ib"];
-$pass = $url["lj70kn5jrdaxl91v"];
-$dbname = substr($url["d99c5oiieri85ca1"], 1);
+$user = $url["vuhsxgawmm76q2m0"];
+$pass = $url["cpe4lruxq8gtnexs"];
+// ใช้ ltrim เพื่อเอาเครื่องหมาย / ออกจาก path
+$dbname = ltrim($url["h335cz5ylaqekthf"], '/');
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
