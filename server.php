d4bfce45998947d2fecef05a9c26d8b38669aefd
Hmster1998 <72091670+Hmster1998@users.noreply.github.com>
2025-02-19 00:12:18 +0700

    Update server.php

--- server.php
+++ server.php
@@ -1,5 +1,5 @@
 <?php
-$url = parse_url(getenv("mysql://c21m3bihcvb3u8ib:lj70kn5jrdaxl91v@ijj1btjwrd3b7932.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/d99c5oiieri85ca1"));
+$url = parse_url(getenv(" mysql://vuhsxgawmm76q2m0:cpe4lruxq8gtnexs@ijj1btjwrd3b7932.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/h335cz5ylaqekthf"));
 $host = $url["ijj1btjwrd3b7932.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"];
 $user = $url["c21m3bihcvb3u8ib"];
 $pass = $url["lj70kn5jrdaxl91v"];
