40137ed86f96df918cfea8b9a81aa10bc4b5e177
Hmster1998 <72091670+Hmster1998@users.noreply.github.com>
2025-02-19 00:00:58 +0700

    Add files via upload

--- server.php
+++ server.php
@@ -1,14 +1,13 @@
 <?php
-$host = "localhost";
-$user = "root";  // Change according to your MySQL settings
-$pass = "";  // Change according to your MySQL settings
-$dbname = "game_shop";
+$url = parse_url(getenv("mysql://c21m3bihcvb3u8ib:lj70kn5jrdaxl91v@ijj1btjwrd3b7932.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/d99c5oiieri85ca1"));
+$host = $url["ijj1btjwrd3b7932.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	"];
+$user = $url["c21m3bihcvb3u8ib"];
+$pass = $url["lj70kn5jrdaxl91v"];
+$dbname = substr($url["d99c5oiieri85ca1"], 1);
 
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error) {
+    die(json_encode(["error" => "Connection failed: " . $conn->connect_error]));
 }
 ?>
-
